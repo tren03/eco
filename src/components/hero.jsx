e5b51efb615de51f8aba7e177a86f0fac72bdcd0
bmsce <vishnusethuraman@gmail.com>
2024-04-20 17:16:00 +0530

    added navbar, hero page, home, need to check mobile view of hero

---
+++ src/components/hero.jsx
@@ -0,0 +1,12 @@
+import './heroStyles.css'
+
+function Hero(){
+    return(
+        <div className="hero-image">
+            <img src="src/assets/hero.jpg"></img>
+        </div>
+        
+    )
+}
+
+export default Hero
---
+++ src/pages/home.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/hero";
+import './homeStyles.css'
+
+function Home(){
+
+    return (
+    <div className="container">
+        <Navbar />
+        <Hero />
+    </div>
+
+    )
+
+}
+
+export default Home
